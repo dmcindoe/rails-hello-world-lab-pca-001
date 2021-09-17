a6ce45b63630a4bbb902d33f27bdd477503af43b
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-17 15:07:07 -0400

    done

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,8 @@
+class StaticController < ApplicationController
+
+  def hello_world
+    # render "layouts/static/hello_world" 
+    # Explicit rendering if not directling pulling from static folder in views
+  end
+    
+end
